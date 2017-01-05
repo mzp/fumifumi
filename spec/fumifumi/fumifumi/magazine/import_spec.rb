99c2d170d34863304a82f40ab34d619e14231c91
mzp <mzpppp@gmail.com>
2017-01-06 08:25:46 +0900

    :lipstick: re-arrange spec structure

--- spec/fumifumi/fumifumi/magazine/import_spec.rb
+++ spec/fumifumi/fumifumi/magazine/import_spec.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Fumifumi::Magazine::Import do
-  let(:magazine) { create(:magazine) }
+  subject { Fumifumi::Magazine::Import.new(magazine).call }
 
-  describe 'count' do
-    subject { Fumifumi::Magazine::Import.new(magazine).call }
-
-    it { expect { subject }.to change(::Page, :count).by(5) }
-  end
-
-  describe 'imported magazine' do
-    subject { Fumifumi::Magazine::Import.new(magazine).call }
-
+  describe '#import' do
+    let(:magazine) { create(:magazine) }
     it do
+      expect { subject }.to change(::Page, :count).by(5)
+
       expect(subject.title).to eq('聖☆おにいさん')
       expect(subject.pages.size).to eq(5)
       expect(subject.pages.map(&:no)).to eq((0..4).to_a)
     end
   end
 
-  xdescribe 'real epub' do
+  xcontext 'real epub' do
     let(:magazine) { create(:magazine, source: Rails.root.join('sample.epub').open) }
-    subject { Fumifumi::Magazine::Import.new(magazine).call }
     it { expect(subject.pages).to_not be_empty }
   end
 end
