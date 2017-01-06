0669b4731353d74babd1a485c4feb3dafcc4e8eb
mzp <mzpppp@gmail.com>
2017-01-06 09:09:13 +0900

    :lipstick: Magazine#episodes returns sorted episode

--- spec/models/magazine_spec.rb
+++ spec/models/magazine_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe Magazine, type: :model do
       expect(subject.cover).to eq(cover)
     end
   end
+
+  describe '#episodes' do
+    let(:magazine) { create(:magazine) }
+    let!(:page2) { create(:page, no: 2, magazine: magazine) }
+    let!(:page1) { create(:page, no: 1, magazine: magazine) }
+    let!(:episode2) { create(:episode, page: page2, magazine: magazine) }
+    let!(:episode1) { create(:episode, page: page1, magazine: magazine) }
+
+    subject { magazine.episodes }
+    it { expect(subject).to eq([episode1, episode2]) }
+  end
 end
