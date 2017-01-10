e2f45412a26aeb5108b561150fcc34de828f3379
mzp <mzpppp@gmail.com>
2017-01-11 01:04:32 +0900

    :computer: add flow-typed

--- config/docker/Dockerfile.js
+++ config/docker/Dockerfile.js
@@ -2,6 +2,10 @@ FROM kkarczmarczyk/node-yarn:7.2-slim
 
 RUN apt update
 
-RUN apt-get install -y libelf1 --no-install-recommends
+RUN apt-get install -y libelf1 git --no-install-recommends
+
+RUN yarn global add flow-typed
+
+RUN flow-typed update-cache
 
 WORKDIR /fumifumi
