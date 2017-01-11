865031b55cf61edb51bbc3b3b92d49a05eee2e4a
mzp <mzpppp@gmail.com>
2017-01-11 12:36:22 +0900

    :coffee: add store and sagas

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,11 +2,15 @@
 import React from "react";
 import {render} from "react-dom";
 import Dropzone from "dropzone";
+import {Provider} from "react-redux";
+import createStore from "store";
 
 window.onload = () => {
     const mountNode : Element = document.getElementById("js");
 
     if (mountNode) {
-        render(<Dropzone />, mountNode);
+        const store = createStore();
+
+        render(<Provider store={store}><Dropzone /></Provider>, mountNode);
     }
 };
