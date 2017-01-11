6d7f11eb9577ba77c57f1e93f1c7e86fd474e893
mzp <mzpppp@gmail.com>
2017-01-11 22:02:24 +0900

    :speaker: show errors on invalid model

--- spec/controllers/magazines_controller_create_spec.rb
+++ spec/controllers/magazines_controller_create_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe MagazinesController, type: :controller do
-  let(:attachment) do
-    fixture_file_upload('files/saint_oniisan.epub', 'application/epub+zip')
-  end
-
   describe '#create' do
     subject do
       post :create, params: {
@@ -12,12 +8,29 @@ RSpec.describe MagazinesController, type: :controller do
       }
     end
 
-    it do
-      expect do
-        subject
-      end.to change(::Magazine, :count).by(1)
-        .and have_enqueued_job(ImportMagazineJob)
-      expect(response.body).to eq('ok'.to_json)
+    context 'epub' do
+      let(:attachment) do
+        fixture_file_upload('files/saint_oniisan.epub', 'application/epub+zip')
+      end
+
+      it do
+        expect do
+          subject
+        end.to change(::Magazine, :count).by(1)
+          .and have_enqueued_job(ImportMagazineJob)
+        expect(response.body).to eq('ok'.to_json)
+      end
+    end
+
+    context 'not epub' do
+      let(:attachment) do
+        fixture_file_upload('files/image.png')
+      end
+
+      it do
+        expect(subject).to be_bad_request
+        expect(subject.body).to match(/Source content type is invalid/)
+      end
     end
   end
 end
