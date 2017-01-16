bdfaef2b1a7511438f6555c80315018a28072780
mzp <mzpppp@gmail.com>
2017-01-17 07:58:44 +0900

    :gem: add json_spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
+  config.include JsonSpec::Helpers
 
   config.example_status_persistence_file_path = 'spec/examples.txt'
   config.filter_rails_from_backtrace!
