a278642da3857474a1cb1563efc069de95fa360b
mzp <mzpppp@gmail.com>
2017-01-17 08:03:46 +0900

    :coffee: add saga for magazine index page

--- front/src/sagas/magazine/index.js
+++ front/src/sagas/magazine/index.js
@@ -1,6 +1,8 @@
 import {fork} from "redux-saga/effects";
 import import_ from "./import";
+import list from "./list";
 
 export default function *() {
     yield fork(import_);
+    yield fork(list);
 }
