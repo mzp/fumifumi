13f11add6037a9779ad06e929d049c4e4532c131
mzp <mzpppp@gmail.com>
2017-01-19 08:24:29 +0900

    :nail_care: add animation for button

--- front/src/components/magazine/list/NavButton.jsx
+++ front/src/components/magazine/list/NavButton.jsx
@@ -22,7 +22,10 @@ export default class extends React.PureComponent {
 
         return (
             <div
-                className={cx(layout, navButton({"hidden": !enable}))}
+                className={cx(layout, navButton({
+                    "hidden": !enable,
+                    "shown": enable
+                }))}
                 onClick={onNav}
             >
                 <div className={navButton("label")}>{label}</div>
