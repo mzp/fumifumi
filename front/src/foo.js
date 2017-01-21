9806c4f270085e09acfdb873a3080b2319961f93
mzp <mzpppp@gmail.com>
2017-01-22 07:30:19 +0900

    :fire: remove unused file

--- front/src/foo.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-const Answer : number = 42;
-
-export default function (): number {
-
-    return Answer;
-
-}
--- front/test/foo-test.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-import test from "ava";
-import foo from "foo";
-
-test((t) => {
-    t.is(foo(), 42);
-});
