ea8d695bc0da8209d02d65f7a77c28e483e38a9a
mzp <mzpppp@gmail.com>
2017-01-21 23:28:48 +0900

    :lipstick: return to dashboard after import action

--- app/controllers/magazines/import_controller.rb
+++ app/controllers/magazines/import_controller.rb
@@ -3,14 +3,16 @@
 module Magazines
   class ImportController < ApplicationController
     def create
+      Magazine.update_all finished_at: nil
       ImportAllMagazineJob.perform_later
-      redirect_to :magazines
+      redirect_to :magazines_dashboard_index
     end
 
     def update
       magazine = Magazine.find(params[:id])
+      magazine.update finished_at: nil
       ImportMagazineJob.perform_later magazine
-      redirect_to :magazines
+      redirect_to :magazines_dashboard_index
     end
   end
 end
