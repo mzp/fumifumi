4e1bec68c2c0ab5b7d7a3d6ec7a382acd3eea699
mzp <mzpppp@gmail.com>
2017-01-28 16:42:14 +0900

    :camera: add option to obtain thumbnail image

--- spec/controllers/pages_controller_show_spec.rb
+++ spec/controllers/pages_controller_show_spec.rb
@@ -2,10 +2,21 @@
 RSpec.describe PagesController, type: :controller do
   let(:page) { create(:page, magazine: create(:magazine)) }
 
-  before { get :show, params: { id: page.id } }
-  subject { response }
-  it do
-    expect(subject.content_type).to eq(page.content_content_type)
-    expect(subject.body).to eq(File.binread(page.content.path))
+  describe 'original' do
+    before { get :show, params: { id: page.id } }
+    subject { response }
+    it do
+      expect(subject.content_type).to eq(page.content_content_type)
+      expect(subject.body).to eq(File.binread(page.content.path))
+    end
+  end
+
+  describe 'thumbnail' do
+    before { get :show, params: { id: page.id, thumbnail: true } }
+    subject { response }
+    it do
+      expect(subject.content_type).to eq(page.content_content_type)
+      expect(subject.body).to eq(File.binread(page.content.path(:thumbnail)))
+    end
   end
 end
