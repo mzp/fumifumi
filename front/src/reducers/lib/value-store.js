9ccb541515f7fb66ae8203eb27baa6cd13757e52
mzp <mzpppp@gmail.com>
2017-01-31 23:23:00 +0900

    :lipstick: fix annotation

--- front/src/reducers/lib/value-store.js
+++ front/src/reducers/lib/value-store.js
@@ -1,7 +1,7 @@
 /* @flow */
 import {handleActions} from "redux-actions";
 
-export default (type : string, init : *) => handleActions({
+export default (type: string, init: *) => handleActions({
     "@@INIT": (state) => state,
     [type]: (state, action) => action.payload
 }, init);
