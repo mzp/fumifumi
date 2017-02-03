23b9074752ee4c52b30b8eb1d252351820b41ee3
mzp <mzpppp@gmail.com>
2017-02-03 09:21:26 +0900

    :lipstick: add bool-store

---
+++ front/src/reducers/lib/bool-store.js
@@ -0,0 +1,13 @@
+/* @flow */
+import {combineActions, handleActions} from "redux-actions";
+
+function next(types: string[], value: bool) {
+  if (types.length === 0) { return {} }
+  return { [combineActions(...types)]: () => value };
+}
+
+export default (enableTypes: array[], disableTypes: array[], init: boolean) => handleActions({
+    "@@INIT": (state) => state,
+    ...next(enableTypes, true),
+    ...next(disableTypes, false)
+}, init);
