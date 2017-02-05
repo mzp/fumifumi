841de9785c63f0fda97a5fed57cc668d04367e1c
mzp <mzpppp@gmail.com>
2017-02-05 14:12:55 +0900

    :lipstick: dispatch fetch action on mounted

--- front/src/components/episode/author/index.jsx
+++ front/src/components/episode/author/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Info from "./Info";
+import b from "components/lib/b";
+import connect from "components/lib/connect";
+import {mainLayout} from "components/layout";
 
+@mainLayout @connect("episode.author")
 export default class extends React.Component {
     static displayName = "Episode.Author"
+
+    static propTypes = {
+        "dispatch": React.PropTypes.func,
+        "params": React.PropTypes.shape({"name": React.PropTypes.string}).isRequired
+    }
+
+    static defaultProps = {}
+
+    componentDidMount () {
+        this.props.dispatch({
+            "payload": this.props.params,
+            "type": "saga.episode.author"
+        });
+    }
 
     render () {
         return <div />;
