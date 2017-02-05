8b35d8b477109479b0cf12d04c0b6503ee7313ce
mzp <mzpppp@gmail.com>
2017-02-05 15:36:34 +0900

    :coffee: show episodes

--- front/src/components/episode/author/index.jsx
+++ front/src/components/episode/author/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Info from "./Info";
+import Page from "./Page";
 import b from "components/lib/b";
 import connect from "components/lib/connect";
+import Types from "components/prop-types";
 import {mainLayout} from "components/layout";
 
 @mainLayout @connect("episode.author")
@@ -10,10 +12,14 @@ export default class extends React.Component {
 
     static propTypes = {
         "dispatch": React.PropTypes.func,
+        "episodes": React.PropTypes.arrayOf(React.PropTypes.shape(Types.episode)),
         "params": React.PropTypes.shape({"name": React.PropTypes.string}).isRequired
     }
 
-    static defaultProps = {}
+    static defaultProps = {
+        "episodes": [],
+        "params": {}
+    }
 
     componentDidMount () {
         this.props.dispatch({
@@ -23,6 +29,23 @@ export default class extends React.Component {
     }
 
     render () {
-        return <div />;
+        const {episodes, "params": {name}} = this.props;
+        const layout = b.with("tileLayout");
+
+        return (
+            <div>
+                <Info name={name} />
+                <div className={layout()}>
+                    {episodes.map((e) =>
+                        <Page
+                            key={e.id}
+                            layout={layout("tile")}
+                            url={e.url}
+                            {...e.page}
+                        />)
+                    }
+                </div>
+            </div>
+        );
     }
 }
