a43ebec4c93bcaa2e36400b40867c8c61b4f8376
mzp <mzpppp@gmail.com>
2017-02-05 14:24:03 +0900

    :lipstick: fix import path

--- front/src/sagas/episode/fetch.js
+++ front/src/sagas/episode/fetch.js
@@ -1,7 +1,7 @@
 /* @flow */
 import {take, put, call} from "redux-saga/effects";
 import action from "actions/episode/show";
-import fetch from "api/fetch-episode";
+import fetch from "api/episode/fetch";
 
 export default function *(): Generator<*, *, *> {
     // eslint-disable-next-line no-constant-condition
