9fcd20cc5943b6e0f09f33c48d58e331d1e25479
mzp <mzpppp@gmail.com>
2017-02-05 15:57:59 +0900

    :bug: Episode#author might be nil

--- app/fumifumi/resource/episode.rb
+++ app/fumifumi/resource/episode.rb
@@ -3,7 +3,7 @@ module Resource
   class Episode < Base
     field :page, with: Resource::Page
     field :url, proc: -> { "/episodes/#{model.id}" }
-    field :author_url, proc: -> { "/episodes/author?name=#{URI.encode(model.author)}" }
+    field :author_url, proc: -> { "/episodes/author?name=#{URI.encode(model.author.to_s)}" }
     array_field :pages, with: Resource::Page
   end
 end
