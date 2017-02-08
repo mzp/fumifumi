8fbe7d38b4f14ef49e97ac819135f0c3aee2be58
mzp <mzpppp@gmail.com>
2017-02-08 19:54:49 +0900

    :lipstick: allow empty connection

--- front/src/components/lib/connect.js
+++ front/src/components/lib/connect.js
@@ -1,6 +1,10 @@
 import {connect} from "react-redux";
 import get from "lodash.get";
 
-export default function (path) {
+export default function (path = null) {
+    if (path === null) {
+        return connect(() => ({}));
+    }
+
     return connect((state) => get(state, path));
 }
