933e2356951aea35b42aaf8be781dff4e257586a
mzp <mzpppp@gmail.com>
2017-02-22 21:48:46 +0900

    :bug: fix migration class name

--- db/migrate/20170222103756_add_original_filename_to_magazines.rb
+++ db/migrate/20170222103756_add_original_filename_to_magazines.rb
@@ -1,4 +1,4 @@
-class AddOriginalFileNameToMagazines < ActiveRecord::Migration[5.0]
+class AddOriginalFilenameToMagazines < ActiveRecord::Migration[5.0]
   def change
     change_table :magazines do |t|
       t.string :original_filename
