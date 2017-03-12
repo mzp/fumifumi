7aa67994984e14add6f596b62c547680550c0388
mzp <mzpppp@gmail.com>
2017-03-13 08:03:02 +0900

    :fire: /magazines page

--- front/src/reducers/magazine/index.js
+++ front/src/reducers/magazine/index.js
@@ -1,9 +1,7 @@
 /* @flow */
 import {combineReducers} from "redux";
 import import_ from "./import";
-import list from "./list";
 
 export default combineReducers({
-    import_,
-    list
+    import_
 });
--- front/src/sagas/magazine/index.js
+++ front/src/sagas/magazine/index.js
@@ -1,8 +1,6 @@
 import {fork} from "redux-saga/effects";
 import import_ from "./import";
-import list from "./list";
 
 export default function *() {
     yield fork(import_);
-    yield fork(list);
 }
