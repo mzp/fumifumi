4065f80323b89ea18728975ab7b8c60fcc1a02d5
mzp <mzpppp@gmail.com>
2017-03-19 15:44:01 +0900

    :muscle: add navigation method

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -18,4 +18,39 @@ RSpec.describe Episode do
       expect(subject.pages.size).to eq(2)
     end
   end
+
+  describe 'navigation' do
+    let(:magazine) { create(:magazine) }
+    let!(:prev_prev) { create(:episode, magazine: magazine) }
+    let!(:prev) { create(:episode, magazine: magazine) }
+    let!(:current) { create(:episode, magazine: magazine) }
+    let!(:next_) { create(:episode, magazine: magazine) }
+    let!(:next_next) { create(:episode, magazine: magazine) }
+
+    before do
+      [
+        prev_prev,
+        prev,
+        current,
+        next_,
+        next_next
+      ].each do |episode|
+        create_list(:page, 2, episode: episode, magazine: magazine)
+      end
+    end
+
+    describe '#next' do
+      subject { current.next }
+      it { expect(subject).to eq(next_) }
+      it { expect(subject.next).to eq(next_next) }
+      it { expect(subject.next.next).to be_nil }
+    end
+
+    describe '#prev' do
+      subject { current.prev }
+      it { expect(subject).to eq(prev) }
+      it { expect(subject.prev).to eq(prev_prev) }
+      it { expect(subject.prev.prev).to be_nil }
+    end
+  end
 end
