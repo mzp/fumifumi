1a5297807de067875fd94055b71b723f66f79553
mzp <mzpppp@gmail.com>
2017-03-19 15:55:04 +0900

    :sparkles: add navigation to response

--- app/fumifumi/resource/episode_detail.rb
+++ app/fumifumi/resource/episode_detail.rb
@@ -3,5 +3,7 @@ module Resource
   class EpisodeDetail < ::Resource::Episode
     field :magazine_title, proc: -> { model.magazine.title }
     field :magazine_url, proc: -> { "/episodes/magazine/#{model.magazine.id}" }
+    field :next, with: Resource::Episode
+    field :prev, with: Resource::Episode
   end
 end
