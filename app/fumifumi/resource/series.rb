20974851737764816a1d608a16ea8a27e2e60f2d
mzp <mzpppp@gmail.com>
2017-04-05 22:07:16 +0900

    :sparkles: add magazines_url to series response

--- app/fumifumi/resource/series.rb
+++ app/fumifumi/resource/series.rb
@@ -3,7 +3,14 @@
 module Resource
   class Series < Base
     field :magazines, proc: lambda {
-      model.magazines.recent.limit(4).map(&Resource::MagazineOverview.method(:new))
+      model.magazines.recent.limit(5).map(&Resource::MagazineOverview.method(:new))
+    }
+    field :magazines_url, proc: lambda {
+      if model.magazines.count < 5
+        nil
+      else
+        api_web_magazines_path(series_id: model.id, page: 2, per_page: 5)
+      end
     }
   end
 end
--- spec/controllers/api/web/series_controller_index_spec.rb
+++ spec/controllers/api/web/series_controller_index_spec.rb
@@ -18,8 +18,11 @@ RSpec.describe Api::Web::SeriesController, type: :controller do
 
   describe '#magazines' do
     it do
-      expect(subject).to have_json_size(4).at_path('0/magazines')
-      0.upto(3) do |i|
+      expect(subject).to have_json_size(5).at_path('0/magazines')
+      expect(subject).to be_json_eql(api_web_magazines_path(series_id: series.first.id, page: 2, per_page: 5).to_json)
+        .at_path('0/magazines_url')
+      expect(subject).to be_json_eql(nil.to_json).at_path('1/magazines_url')
+      0.upto(4) do |i|
         expect(subject).to be_json_eql(magazines[-i - 1].title.to_json).at_path("0/magazines/#{i}/title")
         expect(subject).to have_json_path("0/magazines/#{i}/url")
         expect(subject).to have_json_path("0/magazines/#{i}/cover")
