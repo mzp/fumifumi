2732b99f2eb524f22e79c3cc05c3e6dffa1423a0
mzp <mzpppp@gmail.com>
2017-04-06 08:46:42 +0900

    :coffee: add infinity scroll

--- front/src/components/series/list/index.jsx
+++ front/src/components/series/list/index.jsx
@@ -3,30 +3,61 @@ import ReactPlaceholder from "react-placeholder";
 import Series from "./Series";
 import Placeholder from "./placeholder";
 import {fetch} from "reducers/resource";
+import {fetchNext} from "reducers/pagingResource";
 import {mainLayout} from "components/layout";
 import connect from "components/lib/connect";
 import Types from "components/prop-types";
 
-@mainLayout @connect("series.list.resource")
+@mainLayout @connect("series.list")
 export default class extends React.Component {
     static displayName = "Series.List.index"
     static propTypes = {
-        "data": React.PropTypes.arrayOf(React.PropTypes.shape(Types.series)),
         "dispatch": React.PropTypes.func,
-        "ready": React.PropTypes.bool
+        "magazines": React.PropTypes.objectOf(
+            React.PropTypes.arrayOf(
+              React.PropTypes.shape(Types.magazine)
+        )),
+        "series": React.PropTypes.shape({
+            "data": React.PropTypes.arrayOf(React.PropTypes.shape(Types.series)),
+            "ready": React.PropTypes.bool
+        })
     }
+
     static defaultProps = {
-        "data": [],
         "dispatch": null,
-        "ready": false
+        "magazines": {},
+        "series": {
+            "data": [],
+            "ready": false
+        }
     }
 
     componentDidMount () {
         fetch(this.props.dispatch, "series.list", "/api/web/series");
     }
 
+    handleLoad (id) {
+        const magazines = this.props.magazines[id];
+
+        fetchNext(this.props.dispatch, `series.list.magazines.${id}`, magazines.next);
+    }
+
+    seriesElement (series) {
+        const magazines = this.props.magazines[series.id];
+        const hasMore = !magazines.loading && Boolean(magazines.next);
+
+        return (
+            <Series
+                hasMore={hasMore}
+                key={series.id}
+                magazines={magazines.data}
+                onLoad={() => this.handleLoad(series.id)}
+                title={series.title}
+            />);
+    }
+
     render () {
-        const {data, ready} = this.props;
+        const {data, ready} = this.props.series;
 
         return (
             <ReactPlaceholder
@@ -34,11 +65,7 @@ export default class extends React.Component {
                 ready={ready}
             >
                 <div>
-                    {data.map((series) =>
-                        <Series
-                            key={series.id}
-                            {...series}
-                        />)}
+                    {data.map(::this.seriesElement)}
                 </div>
             </ReactPlaceholder>
         );
