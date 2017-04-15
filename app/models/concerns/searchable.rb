7234f84ffa168c72f3a4b5ca4973591c2f0d5244
mzp <mzpppp@gmail.com>
2017-04-16 07:47:30 +0900

    :jp: add kana fields

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -9,4 +9,18 @@ module Searchable
 
     index_name { "#{name.underscore}_#{Rails.env}" }
   end
+
+  class_methods do
+    def fields_ja(names)
+      ja = {
+        index_analyzer: 'ngram_ja', search_analyzer: 'simple_keyword', fields: {
+          raw: { type: 'string', index: 'not_analyzed' },
+          kana: { type: 'string', index_analyzer: 'kana', search_analyzer: 'simple_keyword' },
+          single: { type: 'string', analyzer: 'ngram_single' }
+        }
+      }
+
+      mapping { names.each { |name| indexes name, ja } }
+    end
+  end
 end
